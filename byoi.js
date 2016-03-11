3759b58ea4b0589614ea9bab96559ecfb9821dcb
Chris Harris <zbac010@live.rhul.ac.uk>
2016-03-11 15:08:38 +0000

    update, fullscreen working with spinning pref. icon

---
+++ byoi.js
@@ -0,0 +1,39 @@
+var main = function() {
+  /* Global Variables */
+  var preferencesState = 0;
+
+  /* Hides the preferencesMenu on page load */
+  $(".preferencesMenu").hide();
+
+  /* Sends the string in the messageBox when the sendButton is clicked */
+  $('.sendButton').click(function() {
+    $('.messageField').val($('.textBox').val());
+  });
+
+  /* Rotates the preferencesCog when its clicked to wind and unwind the preferencesMenu */
+  $("#preferencesCog").rotate({
+    bind: {
+      click: function() {
+        if (preferencesState === 0) {
+          $(this).rotate({
+            animateTo: 180
+          });
+          preferencesState = 1;
+        } else {
+          $(this).rotate({
+            animateTo: 0
+          });
+          preferencesState = 0;
+        }
+      }
+    }
+  });
+
+  /* Toggles if the preferencesMenu is displayed when the preferencesButton is clicked */
+  $(".preferencesButton").click(function() {
+    $(".preferencesMenu").slideToggle('slow');
+  });
+
+};
+
+$(document).ready(main);
